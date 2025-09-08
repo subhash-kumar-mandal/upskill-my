94e7e229d968fa0c264401c99578623cf30e498a
subhash-kumar-mandal <pm817405@gmail.com>
2025-09-08 11:26:18 +0530

    check palindrome

--- linear-search/index-2.js
+++ linear-search/index-2.js
@@ -5,17 +5,37 @@
 // Input: str = "subhash"
 //  target = "h"  
 // Output: 3
-let string1= "subhashkumarmandal";
-let tar1 =  "h"
-console.log(tar1)
-function TextCount(arr){
-    let TextCount =0
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]==tar1 ){
-          TextCount++
+let string1 = "subhashkumarmandal";
+let tar1 = "h"
+function TextCount(arr) {
+    let TextCount = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == tar1) {
+            TextCount++
         }
     }
-    return console.log(TextCount)
+    return (TextCount)
 }
 
 // TextCount(string1)
+
+
+// question 2  Palindrome Check (Linear Search logic से)
+// Input: arr = [1, 2, 3, 2, 1]
+// Output: true
+
+let arr = [1,2,3,2,1]
+function find_palindrome(arra) {
+    let check = true;
+    for (let i = 0; i < arra.length; i++) {
+        let j = arra.length-1 - i
+        if (arra[i] !== arra[j]) {
+            return check = false
+        }
+    }
+    return check ;
+}
+
+
+console.log(find_palindrome(arr))
+
