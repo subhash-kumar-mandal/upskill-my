4d3f476dacc92c9b1eb15ccf9492fa74556b10b2
subhash-kumar-mandal <pm817405@gmail.com>
2025-09-10 12:09:38 +0530

    Count frequency of element

--- basics-js/index-1.js
+++ basics-js/index-1.js
@@ -46,3 +46,57 @@ function findmax(array){
 
 // console.log(arraysum([1,2,3,4,5,6])) ----> 21
 
+
+
+// Question 4 find average in array 
+let findaverge = (array) => {
+     let sum = 0;
+     let averge = 0;
+    for (let j = 0; j < array.length;j++) {
+        sum += array[j]
+        }
+     averge = (sum/array.length)
+     return `sum = ${sum} and averge = ${averge}` // here is return averge
+}
+
+// console.log(findaverge([1,2,3,4,5,6]))
+
+
+
+// Question 5. Count frequency of element 
+//   input --> arr =[1,1,2,3,4,5,5,5,6,7]
+//   output --->  1-->2
+//                2-->1
+//                3-->1
+//                4-->1
+//                5-->3
+//                6-->1
+//                7-->1
+ 
+let CountFrequency = (arr) => {
+let checkarra =[]
+  for(let i=0;i<arr.length;i++){
+     let count = 0
+     let  check = false;
+     for(let k=0;k<checkarra.length;k++){
+        if(arr[i]===checkarra[k]){
+            check =true;
+            break ;
+        }
+     }
+     if(check){
+        continue ;
+     }
+
+     for(let f=0; f<arr.length ;f++){
+        if(arr[f] === arr[i]){
+            count++
+        }
+     }
+     console.log(`${arr[i]} ---> ${count}`)
+     checkarra.push(arr[i])
+  }
+
+}
+
+CountFrequency([1,1,2,3,4,5,5,5,6,7])
