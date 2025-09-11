73e94498d871c814513dddc696cdd1746481200b
subhash-kumar-mandal <pm817405@gmail.com>
2025-09-11 12:08:29 +0530

    updated

--- basics-js/index-2.js
+++ basics-js/index-2.js
@@ -13,3 +13,27 @@ let linearfind= (arr,tar) =>{
     }
 
 // console.log(linearfind([1,2,3,4,5,6],8))
+
+// 2. Binary Search  ---->
+ let bs = (array,tar) =>{
+    let low = 0;
+    let high =array.length-1                             
+      for(let i=0;i<array.length;i++){
+         let mid = Math.floor((low+high)/2);
+         if(array[mid] ===tar){
+               return `yes element find index ${mid} `  // low = mid +1 , high = mid -1
+         } else if (array[mid]<tar){
+            low = mid+1 ;
+         } else{
+            high = mid -1 ;
+         }
+      }
+
+      return -1;
+ }
+
+//  console.log(bs([1,2,3,4,5,6],9))
+
+
+
+
