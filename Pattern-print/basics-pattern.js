67c2215b8f69ef7ad430b1fe6da8915f114f8c07
subhash-kumar-mandal <pm817405@gmail.com>
2025-09-13 13:55:02 +0530

    square pattern

---
+++ Pattern-print/basics-pattern.js
@@ -0,0 +1,19 @@
+// ----------------------
+// basics pattern 
+// ----------------------
+
+//   Question 1. Square Pattern 
+   
+    // *****
+    // *****
+    // *****
+    // *****
+    // *****
+
+    for(let i=0 ;i<5;i++){
+        let star=""
+        for(let j=0 ;j<5;j++){
+           star += "*"
+        }
+        console.log(star)
+    }
