9c6b03d2b7c9a462532b1bffb30d7361add06299
subhash-kumar-mandal <pm817405@gmail.com>
2025-09-14 12:32:24 +0530

    Number Triangle

---
+++ Pattern-print/number-pathern.js
@@ -0,0 +1,29 @@
+// ----------------------------
+// Level 2: Number Patterns
+// ----------------------------
+
+// Question 4 Number Triangle 
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+for(let i=1 ;i<=5;i++){
+    let str=""
+    for(let j =1 ;j<=5;j++){
+      if(j<=i){
+        str += `${j}`
+      }
+    }
+    console.log(str)
+}
+
+
+// Question 2 Inverted Number Triangle 
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
