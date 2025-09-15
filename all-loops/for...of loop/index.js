8398b2745b633a02c3f120f4c1c10040c3a0ebef
subhash-kumar-mandal <pm817405@gmail.com>
2025-09-15 21:05:47 +0530

    question 1

---
+++ all-loops/for...of loop/index.js
@@ -0,0 +1,22 @@
+// --------------------------------
+// 🔹 for...of Loop in JavaScript
+// --------------------------------
+
+// 👉 for...of loop iterable objects (जैसे array, string, Map, Set) पर चलाने के लिए use होता है।
+// 👉 ये directly value देता है, index नहीं।
+
+// Syntax ---->
+//   for (let variable of iterable) {
+//    // code block
+// }
+    //   1. variable → हर बार एक value store करता है
+    //   2. iterable → कोई भी iterable data structure (Array, String, Set, Map आदि)
+
+
+    //   ------> question  leve 1 
+
+let num = [1,2,3,4,5,6]
+for(let element of num){
+    // console.log(element)  // print the element 
+}
+
