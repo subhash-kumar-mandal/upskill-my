20a77ec7bc54adf72556119764a0e2f597c9e584
subhash kumar mandal <pm817405@gmail.com>
2025-10-03 13:59:16 +0530

    question 1

--- all-loops/while-loop/base-2.js
+++ all-loops/while-loop/base-2.js
@@ -1,3 +1,16 @@
 // ---------
 // level 2 
 // ---------
+
+function sum(a){
+    let sum =0 ;
+    let count=1;
+    while(count<=a){
+        sum = sum+count;
+        count++
+    }
+    console.log(sum)
+}
+
+// sum(100)
+
