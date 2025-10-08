65eabe4ce9504e63ad29e5da4b1cdf017be594ab
subhash-kumar-mandal <pm817405@gmail.com>
2025-10-08 11:55:11 +0530

    bug fix

--- arrays/basic-methods/index.js
+++ arrays/basic-methods/index.js
@@ -23,3 +23,28 @@ console.log(fruits); // ["apple", "banana", "mango"]
 // Example 2: pop()
 //     array ko change karte hai 
 //     pop() array ke last se remove karta hai element 
+
+let fruit = ["apple", "banana", "mango"];
+fruit.pop();
+console.log(fruit); // ["apple", "banana"]
+
+
+// Example 3: unshift()
+//     array ko change karte hai 
+//     unshift() array ke start mein add karta hai element 
+
+
+let fruits_name = ["banana", "mango"];
+fruits_name.unshift("apple");
+console.log(fruits_name); // ["apple", "banana", "mango"]
+
+
+// Example 4: shift()
+//     array ko change karte hai 
+//     shift() array ke start se remove karta hai element 
+
+let fruit1 = ["apple", "banana", "mango"];
+fruit1.shift();
+
+console.log(fruit1); // ["banana", "mango"]
+
