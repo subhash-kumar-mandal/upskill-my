2dcdc2d9880f38ebcf874c2cae56beddbf04a333
subhash-kumar-mandal <pm817405@gmail.com>
2025-10-09 21:22:14 +0530

    check string

--- all-loops/for...of loop/index-1.js
+++ all-loops/for...of loop/index-1.js
@@ -17,5 +17,19 @@ const find =(arra)=>{
 find([12,32,4,56,78,89])
 
 
+// 2.check count vowel and consonants in string
 
+const checkstr = (string)=>{
+    let vowel =0;
+    let consonants =0;
+    for(let str of string){
+        if( str ==="a"||str ==="u"||str ==="e"||str ==="i"||str ==="o"){
+            vowel++
+        }else[
+            consonants++
+        ]
+    }
+    console.log(`vowels->${vowel} and consonants->${consonants}`)
+}
 
+checkstr("hello")
