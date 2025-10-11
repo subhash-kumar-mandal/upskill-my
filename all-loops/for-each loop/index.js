2df273f078d09315b824c389470d61decab2e00c
subhash-kumar-mandal <pm817405@gmail.com>
2025-10-11 12:19:02 +0530

    element double print

--- all-loops/for-each loop/index.js
+++ all-loops/for-each loop/index.js
@@ -4,3 +4,9 @@ const numbers = [5, 10, 15, 20];
 numbers.forEach((Element,index,arra) =>{
     console.log(Element)
 })
+
+// 2. number ko double karo haar element 
+const num = [1,2,35,56,75];
+num.forEach((Element)=>{
+    console.log(Element+Element)
+})
