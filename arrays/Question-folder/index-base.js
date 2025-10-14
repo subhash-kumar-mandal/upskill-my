bb9c9b6648f9b28945e5125c83c18c46cf691129
subhash-kumar-mandal <pm817405@gmail.com>
2025-10-14 19:28:50 +0530

    practice

---
+++ arrays/Question-folder/index-base.js
@@ -0,0 +1,35 @@
+// practice (push, pop, shift, unshift, indexOf, includes)
+let fruits = [];
+fruits.push("apple");
+fruits.push("banana");
+fruits.push("mango");
+fruits.pop();
+console.log(fruits)
+
+
+let num = [10,20,30];
+num.unshift(5);
+num.shift();
+console.log(num);
+
+
+let check_element =(arra)=>{
+    if(arra.includes("banana")){
+        console.log("found");
+    }else{
+        console.log("not found")
+    }
+}
+
+check_element([ 'apple', 'banana', 'mango' ]) // "found";
+
+
+let number =[100, 200, 300, 400];
+if(number.indexOf(300) >= 0 ){
+console.log(number.indexOf(300))
+}else{
+    console.log("not found ")
+}
+
+
+let name =["subhash"]
