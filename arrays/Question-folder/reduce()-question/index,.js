63fb5453f74a55a79d9f120f85ad7eaa0616b568
subhash-kumar-mandal <pm817405@gmail.com>
2025-10-18 19:45:55 +0530

    reduce

---
+++ arrays/Question-folder/reduce()-question/index,.js
@@ -0,0 +1,3 @@
+// ----> what is reduce method 
+//    reduce() array के सारे elements को एक single value में combine कर देता है।
+//   sum निकालना, average, product, max, min, या object बनाना — सब reduce से किया जा सकता है।
