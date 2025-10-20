fc80ff9bfd641cbe921c1dfa8c242502684f6a48
subhash-kumar-mandal <pm817405@gmail.com>
2025-10-20 14:35:08 +0530

    index find

---
+++ arrays/Question-folder/findindex()-question/index.js
@@ -0,0 +1,19 @@
+
+// 🔹 findIndex()
+// findIndex() किसी array में पहला ऐसा element का index लौटाता है जो condition को पूरा करे।
+// अगर कोई भी match नहीं मिलता, तो ये -1 return करता है।
+// ध्यान दो: ये element नहीं, index return करता है।
+// 🔹 Syntax:
+// array.findIndex(function(element, index, array) {
+//   // condition return true or false
+// });
+
+
+
+// Question 1: Number > 20
+const numbers = [5, 12, 18, 25, 30];
+let num = numbers.findIndex((element)=>{
+    return  element >20
+})
+
+console.log(num)
