4721e1822450f805e1f6c14e63ca0ccbd5a8d5fb
subhash-kumar-mandal <pm817405@gmail.com>
2025-10-20 13:17:13 +0530

    find method rules

---
+++ arrays/Question-folder/find()-question/index.js
@@ -0,0 +1,24 @@
+// 🔹 find()
+
+// find() किसी array में पहला ऐसा element लौटाता है जो दिए गए condition को पूरा करता है।
+// अगर कोई भी element condition match नहीं करता, तो ये undefined देता है।
+
+// 🔹 Syntax:
+// array.find(function(element, index, array) {
+//   // condition return true or false
+// });
+
+// exmple
+const numbers = [10, 25, 30, 45, 60];
+
+const result = numbers.find(num => num > 40);
+// 40 se bada number dekhna hai but find first condition se true hote hi first element return karta hai 
+// yadi  condition true nhi hoti to undefined dena 
+
+console.log(result);
+ 
+// ex 
+const nums = [1, 2, 3, 4];
+const res = nums.find(num => num > 10);
+console.log(res);
+
